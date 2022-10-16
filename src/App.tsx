accd8bf9d20fa27c1ee5939a86cb96bd93463ced
RunyShark <spartadragon117@live.com.mx>
2022-10-16 16:55:43 -0500

    poke

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import {Text} from 'react-native';
 
-const App = () => {
-  return <View></View>;
+import {NavigationContainer} from '@react-navigation/native';
+import {Navigator} from './navigator/Navigator';
+import {Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Wenas</Text>
+    </View>
+  );
 };
 
 export default App;
