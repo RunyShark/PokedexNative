21ea801d134c38278afda67016c2220b6a84474c
RunyShark <spartadragon117@live.com.mx>
2022-10-16 22:42:42 -0500

    details

--- src/screen/Pokemon/PokemonStyle.ts
+++ src/screen/Pokemon/PokemonStyle.ts
@@ -1,3 +1,16 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {},
+  hederContainer: {
+    height: 370,
+    zIndex: 999,
+    alignItems: 'center',
+    borderBottomLeftRadius: 1000,
+    borderBottomRightRadius: 1000,
+  },
+  backBtn: {
+    position: 'absolute',
+    left: 20,
+  },
+});
--- src/screen/Pokemon/index.tsx
+++ src/screen/Pokemon/index.tsx
@@ -1,19 +1,33 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {RootStackParams} from '../../index';
+import {styles} from './PokemonStyle';
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> {}
 
-export const PokemonScreen = (props: Props) => {
+export const PokemonScreen = ({navigation, route}: Props) => {
+  const {top} = useSafeAreaInsets();
   const {
+    color,
     simplePokemon: {id, name, picture},
-  } = props.route.params;
+  } = route.params;
+
   return (
     <View>
-      <Text>{id}</Text>
-      <Text>{name}</Text>
-      <Text>{picture}</Text>
+      <View
+        style={{
+          ...styles.hederContainer,
+          backgroundColor: color,
+        }}>
+        <TouchableOpacity
+          style={{...styles.backBtn, top: top + 10}}
+          onPress={() => navigation.pop()}>
+          <Icon name="arrow-back-outline" color="white" size={35} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
