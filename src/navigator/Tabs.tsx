30f4d94eaf93f7f9269f4c56e4194de11dd35b2f
RunyShark <spartadragon117@live.com.mx>
2022-10-18 21:20:22 -0500

    icon

--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -1,15 +1,43 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {SearhScreen, Navigator} from '../index';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Navigator" component={Navigator} />
-      <Tab.Screen name="SearhScreen" component={SearhScreen} />
+    <Tab.Navigator
+      sceneContainerStyle={{backgroundColor: 'white'}}
+      tabBarOptions={{
+        activeTintColor: '#5856D6',
+        labelStyle: {marginBottom: 20},
+        style: {
+          height: 60,
+        },
+      }}>
+      <Tab.Screen
+        name="Navigator"
+        component={Navigator}
+        options={{
+          tabBarLabel: 'Listado',
+          tabBarIcon: ({color}) => (
+            <Icon color={color} size={25} name="list-outline" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SearhScreen"
+        component={SearhScreen}
+        options={{
+          tabBarLabel: 'Listado',
+          tabBarIcon: ({color}) => (
+            <Icon color={color} size={25} name="search-outline" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
