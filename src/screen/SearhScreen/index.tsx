02aa32b08c763022d48e471974826914737b1eaa
RunyShark <spartadragon117@live.com.mx>
2022-10-22 21:06:46 -0500

    search

--- src/screen/SearhScreen/index.tsx
+++ src/screen/SearhScreen/index.tsx
@@ -1,17 +1,36 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Dimensions, FlatList, Platform, Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {Loading, PokemonCard, SearchInput, usePokemonSearch} from '../../index';
+import {
+  Loading,
+  PokemonCard,
+  SearchInput,
+  SimplePokemon,
+  usePokemonSearch,
+} from '../../index';
 
 import {styles} from './SearchStyle';
 
 const {width: screenWidth} = Dimensions.get('window');
 
 export const SearhScreen = () => {
+  const [pokemonFilter, setPokemonFilter] = useState<SimplePokemon[]>([]);
   const [term, setTerm] = useState('');
   const {top} = useSafeAreaInsets();
   const {isFetching, simplePokemon} = usePokemonSearch();
+
+  useEffect(() => {
+    if (term.length === 0) {
+      return setPokemonFilter([]);
+    }
+    setPokemonFilter(
+      simplePokemon.filter(poke =>
+        poke.name.toLowerCase().includes(term.toLowerCase()),
+      ),
+    );
+  }, [term]);
 
   if (isFetching) {
     return <Loading />;
@@ -33,7 +52,7 @@ export const SearhScreen = () => {
         }}
       />
       <FlatList
-        data={simplePokemon}
+        data={pokemonFilter}
         numColumns={2}
         showsVerticalScrollIndicator={false}
         renderItem={({item}) => <PokemonCard {...item} />}
