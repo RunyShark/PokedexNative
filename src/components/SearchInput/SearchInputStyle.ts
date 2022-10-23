f724754533a33b9bc19a7de340ee0b28944de30e
RunyShark <spartadragon117@live.com.mx>
2022-10-22 19:31:39 -0500

    seach

--- src/components/SearchInput/SearchInputStyle.ts
+++ src/components/SearchInput/SearchInputStyle.ts
@@ -1,7 +1,28 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'red',
+  container: {},
+  textBackground: {
+    backgroundColor: '#F3F1F3',
+    borderRadius: 50,
+    height: 40,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8,
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 18,
+    top: 2,
   },
 });
--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
 import {styles} from './SearchInputStyle';
 
 export const SearchInput = () => {
   return (
-    <View>
-      style={styles.container}
-      <Text>SearchInput</Text>
+    <View style={styles.container}>
+      <View style={styles.textBackground}>
+        <TextInput
+          placeholder="Search Pokemon"
+          style={styles.textInput}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Icon name="search-outline" color="grey" size={30} />
+      </View>
     </View>
   );
 };
